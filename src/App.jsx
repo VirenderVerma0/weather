875542d0511aaf867801cf68291f1d00c0e87ef8
Virender Verma <virenderv111@gmail.com>
2025-05-10 14:34:00 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Weather from './components/weather'
+import Weather from "./components/Weather"
 import Navbar from './components/Navbar'
 import About from './components/About'
 import { Route, Router, Routes } from 'react-router-dom'
